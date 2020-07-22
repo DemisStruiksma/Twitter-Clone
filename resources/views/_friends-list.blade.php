f25555f9010b1883d0059ae057982be4136e59fe
Demis Struiksma <demis@thenextweb.com>
2020-07-22 16:23:29 +0200

    Built following functionallity

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,14 +1,14 @@
-<h3 class="font-bold text-xl mb-4">Friends</h3>
+<h3 class="font-bold text-xl mb-4">Following</h3>
 
 <ul>
-    @foreach(range(1, 8) as $index)
+    @foreach(auth()->user()->follows as $user)
     <li>
         <div class="flex items-center mb-4">
             <img 
-            src="https://i.pravatar.cc/40" 
+            src="{{ $user->avatar }}" 
             alt=""
             class="rounded-full mr-2">
-            John Doe
+            {{ $user->name }}
         </div>
     </li>
     @endforeach

1189a021f176980552fcce5f142efad3ffdf2cec
Demis Struiksma <demis@thenextweb.com>
2020-07-28 14:48:42 +0200

    Give the welcome page some Tailwind <3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,78 +3,24 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <title>Twitty</title>
+        <link href="/css/main.css" rel="stylesheet">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-
-
-            <div class="content">
-                <div class="title m-b-md">
+        <section class="px-8 py-4 mb-6">
+            <header class="container mx-auto">
+                <h1 class="text-4xl">🐦</h1>
+            </header>
+        </section>
+            <div class="container mx-auto flex justify-center">
+                <div class="text-6xl font-bold">
                     Twitty
                 </div>
-
-                <div class="links">
+            </div>
+                <div class="container mx-auto flex justify-center">
+                    <div class="mt-4 space-x-4 text-lg">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ route('home') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
@@ -82,8 +28,7 @@
                             <a href="{{ route('register') }}">Register</a>
                         @endif
                     @endauth
+                    </div>
                 </div>
-            </div>
-        </div>
-    </body>
-</html>
+            </body>
+        </html>

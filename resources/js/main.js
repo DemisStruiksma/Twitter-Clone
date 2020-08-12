fc09d4ea042fb3c5a4d924775dd2a8593441d978
Demis Struiksma <demis@thenextweb.com>
2020-08-12 16:26:10 +0200

    Add JS for tweet image preview

--- resources/js/main.js
+++ resources/js/main.js
@@ -11,3 +11,23 @@ $('#tweet_body').keyup(function() {
 });
 
 $('div.alert').not('.alert-important').delay(3000).fadeOut(350);
+
+$('#preview').hide();
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    
+    reader.onload = function(e) {
+      $('#preview').attr('src', e.target.result);
+    }
+    
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+$("#image").change(function() {
+  $('#preview').show();
+  readURL(this);
+});
+
